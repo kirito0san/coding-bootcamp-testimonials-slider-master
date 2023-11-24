d70cc48776ab3733fa217e3318894dad0d7f923b
kirito0san <jodat.ahmedsyed@gmail.com>
2023-11-24 10:18:15 +0200

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,17 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'pattern': "url('./images/pattern-bg.svg')",
-        'pattern-down': "url('./images/pattern-curve.svg')",
+        pattern: "url('../images/pattern-bg.svg')",
+        "pattern-down": "url('../images/pattern-curve.svg')",
       },
       colors: {
         Dark_Blue: "hsl(240, 38%, 20%)",
-        Grayish_Blue: "hsl(240, 18%, 77%)"
+        Grayish_Blue: "hsl(240, 18%, 77%)",
       },
       fontFamily: {
-        'Inter': ['Inter', 'sans-serif'],
-      }
+        Inter: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
