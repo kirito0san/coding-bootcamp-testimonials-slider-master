50b55dd2ad00e41a122b523e470ac6de190901df
kirito0san <jodat.ahmedsyed@gmail.com>
2023-11-24 09:37:58 +0200

    S

--- dist/Ts.js
+++ dist/Ts.js
@@ -3,7 +3,6 @@ let btn = document.querySelectorAll(`.btn`);
 let parents = document.querySelectorAll(`.parent`);
 btn.forEach((e) => {
     e.addEventListener(`click`, () => {
-        console.log(1);
         if (parents[1].classList.contains(`!hidden`)) {
             parents[1].classList.remove(`!hidden`);
             parents[0].classList.add(`!hidden`);
